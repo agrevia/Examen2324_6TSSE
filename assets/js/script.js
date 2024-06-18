0fcdcc9aed05b9ab2f02efd1ba3df32d5d986686
agrevia <40198416+agrevia@users.noreply.github.com>
2024-06-18 18:21:16 +0200

    Initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,40 @@
+"use strict";
+//Schrijf hier de nodige variabelen
+
+document.addEventListener('DOMContentLoaded', init);
+
+function init() {
+
+
+
+}
+
+//Functie bij het drukken op de knop "volgende"
+function nextPage(){
+
+}
+
+//Functie bij het drukken op de knop "Item toevoegen"
+function addItem(){
+
+}
+
+//Functie bij het drukken op de knop "Leegmaken"
+function clearButton(){
+
+}
+
+// Verwijder een item als er op wordt geklikt
+function groceryList(){
+
+}
+
+//Haal alle items op uit de localstorage
+function populateGroceryList() {
+
+}
+
+//Update de boodschappenlijst
+function updateLocalStorage() {
+    
+}
